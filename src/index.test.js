3843315f2b23efe26c023d5e4be05c324dd10299
Sanjeevkumar Panicker <Sanjeevkumar.Panicker@cushwake.com>
2017-03-03 18:04:56 -0500

    fourth version with travis ci

--- src/index.test.js
+++ src/index.test.js
@@ -11,11 +11,11 @@ describe('Our first test', () => {
 
 describe('index.html', () => {
 	"use strict";
-	it('should say hello', () => {
+	it('should say hello', (done) => {
 		const index = fs.readFileSync('./src/index.html', 'utf-8');
-		jsdom.env(index, (err, window) => {
+		jsdom.env(index, function (err, window) {
 			const h1 = window.document.getElementsByTagName('h1')[0];
-			expect(h1.innerHTML).to.equal("Hellosdgdsg World!");
+			expect(h1.innerHTML).to.equal("Hello World Test!");
 			done();
 			window.close();
 		});
